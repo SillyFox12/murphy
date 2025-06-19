9c0135e4353c8ec82a855f0885b63b0ff86210d8
SillyFox12 <mykaikmiler@gmail.com>
2025-06-19 10:00:30 -0400

    Verifys if either a webcam or video file can be opened

--- main/utils/webcam.py
+++ main/utils/webcam.py
@@ -13,8 +13,8 @@ class Webcam:
         self.output_dir = output_dir
         self.csv_file = csv_file
         self.ensure_directory(self.output_dir)
-        print(f"Initializing Webcam with output_dir={output_dir}, csv_file={csv_file}")
-    
+        print(f"Initializing Webcam with video_source={video_source}, output_dir={output_dir}, csv_file={csv_file}")
+        self.capture_frames(video_source, output_dir, csv_file)
     #Create output directory if it does not exist
     @staticmethod
     def ensure_directory(path=None):
@@ -32,15 +32,20 @@ class Webcam:
     #Capture frames from the webcam and save them as images. Alternatively, captures frames from a video file
     def capture_frames(self, video_source, output_dir: str, csv_file: str):
         #video_source can be a webcam index (0 for the default webcam) or a video file path
-        
-       # cap = cv2.VideoCapture(0 if video_source is None else video_source)
+        cap = cv2.VideoCapture(0 if video_source is None else video_source)  # type: ignore
 
-       # if not cap.isOpened():
-       #     print(f"Error: Could not open video source {video_source}")
-       #     return
-
-    
-
-
-
-       
+        # Check if the video source is opened successfully
+        if video_source is None:
+            # If no video source is provided, use the default webcam
+            print(f"No video source provided, using live feed.")
+            #Check if the default webcam is opened successfully
+            if not cap.isOpened():
+                print("Error: Could not open video source.")
+                return
+        else:
+            # If a video source is provided, use it
+            print(f"Using video source: {video_source}")
+            # Check if the video source is opened successfully
+            if not cap.isOpened():
+                print(f"Error: Could not open video source {video_source}.")
+                return
