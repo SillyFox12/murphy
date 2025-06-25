fc799776e15efab818215eb538746aa29b4c3954
SillyFox12 <mykaikmiler@gmail.com>
2025-06-25 14:55:17 -0400

    Created audio and video recorder

---
+++ main/utils/recorder.py
@@ -0,0 +1,50 @@
+# ffmpeg_recorder.py
+import subprocess
+import os
+from dir_manager import DirectoryCreator
+from device_select import DeviceLister
+
+class AVRecorder:
+    #Initialize arguments
+    def __init__(self, video_device: str, audio_device: str, dir_manager : DirectoryCreator, framerate=30, duration=None):
+        self.video_device = video_device
+        self.audio_device = audio_device
+        self.output_dir = dir_manager.get_output_dir()
+        self.framerate = framerate
+        self.duration = duration  # in seconds (optional)
+    #Variables for video
+    def build_command(self, filename: str):
+        video_input = f'video="{self.video_device}"'
+        audio_input = f'audio="{self.audio_device}"'
+        output_file = os.path.join(self.output_dir, f"{filename}.mp4")
+        #Arra containing the elements of the ffmpeg command.
+        cmd = [
+            "ffmpeg",
+            "-f", "dshow",
+            "-i", f"{video_input}:{audio_input}",
+            "-t", str(self.duration),
+            "-c:v", "libx264",
+            "-c:a", "pcm_s16le", #WAV audio
+            "-strict", "experimental",
+            "-pix_fmt", "yuv420p",
+            output_file
+        ]
+
+        return cmd
+    #Saves the recording as a file
+    def record(self, filename="output"):
+        command = self.build_command(filename)
+        print("Running FFmpeg command:")
+        print(" ".join(command))
+        subprocess.run(" ".join(command), shell=True)
+
+if __name__ == "__main__":
+   dir_mgr = DirectoryCreator(output_dir="./data")
+   recorder = AVRecorder(
+       video_device="HP HD Camera",
+       audio_device="Internal Microphone (Conexant ISST Audio)",
+       dir_manager=dir_mgr,
+       framerate=30,
+       duration=10
+   )
+   recorder.record(filename="test_recording")
