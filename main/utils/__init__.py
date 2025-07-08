f93cfaf2c1e22781db85f07bdfcc87ce7fe3a418
SillyFox12 <mykaikmiler@gmail.com>
2025-07-08 08:53:38 -0400

    Removed frame.py script as frames now analyzed in memory

--- main/utils/__init__.py
+++ main/utils/__init__.py
@@ -1,8 +1,7 @@
 #Imports utilities to make utility library
 from .device_select import DeviceLister
 from .dir_manager import DirectoryCreator
-#Audio and frame extraction
+#Audio extraction
 from .audio import Microphone
-from .frame import FrameExtractor
 #Webcam recorder with audio
 from .recorder import AVRecorder
